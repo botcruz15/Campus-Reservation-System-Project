498d1196a86de1a6dfa78b28d70e4933459756f1
RolandAaron <botcruz15@gmail.com>
2026-09-16 17:37:42 -0500

    Update WaitingList.h

--- WaitingList.h
+++ WaitingList.h
@@ -2,33 +2,61 @@
 #define WAITING_LIST_H
 
 #include <string>
+
+
 struct WaitingNode {
+
     std::string studentID;
     std::string resourceID;
     std::string timeSlot;
+
     WaitingNode* next;
-}; 
+};
+
+
 class WaitingList {
+
 private:
+
     WaitingNode* front;
     WaitingNode* rear;
+
     int count;
 
+
 public:
-  WaitingList();
-  ~WaitingList();
 
-  void enqueue(const std::string& studentID,
-               const std::string& resourceID,
-               const std::string& timeSlot);
+    WaitingList();
 
-  bool dequeue(WaitingNode& removedStudent);
+    ~WaitingList();
 
-  void displayWaitingList() const;
 
-  bool isEmpty() const;
+    void enqueue(
+        const std::string& studentID,
+        const std::string& resourceID,
+        const std::string& timeSlot
+    );
 
-  int getCount() const;
+
+    bool dequeue(
+        WaitingNode& removedStudent
+    );
+
+
+    bool dequeueForResource(
+        const std::string& resourceID,
+        WaitingNode& removedStudent
+    );
+
+
+    void displayWaitingList() const;
+
+
+    bool isEmpty() const;
+
+
+    int getCount() const;
 };
+
 
 #endif
