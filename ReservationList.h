03dbfcd6c24054d951fd2c5cdaabcead95816613
RolandAaron <botcruz15@gmail.com>
2026-09-16 19:35:42 -0500

    Update ReservationList.h

--- ReservationList.h
+++ ReservationList.h
@@ -3,34 +3,69 @@
 
 #include <string>
 
+
 struct ReservationNode {
+
     std::string reservationID;
     std::string studentID;
     std::string resourceID;
     std::string timeSlot;
+
     ReservationNode* next;
 };
 
+
 class ReservationList {
+
 private:
+
     ReservationNode* head;
     int count;
 
+
 public:
+
     ReservationList();
+
     ~ReservationList();
 
-    void insertReservation(const std::string& reservationID,
-                            const std::string& studentID,
-                            const std::string& resourceID,
-                            const std::string& timeSlot);
-    bool removeReservation(const std::string& reservationID, ReservationNode& removedOut);
-    bool findReservation(const std::string& reservationID, ReservationNode& out) const;
-    bool exists(const std::string& reservationID, 
-                const std::string& timeSlot     
-            ) const;
+
+    void insertReservation(
+        const std::string& reservationID,
+        const std::string& studentID,
+        const std::string& resourceID,
+        const std::string& timeSlot
+    );
+
+
+    bool removeReservation(
+        const std::string& reservationID,
+        ReservationNode& removedOut
+    );
+
+
+    bool findReservation(
+        const std::string& reservationID,
+        ReservationNode& out
+    ) const;
+
+
+    bool exists(
+        const std::string& reservationID
+    ) const;
+
+
+    int countReservationsForSlot(
+        const std::string& resourceID,
+        const std::string& timeSlot
+    ) const;
+
+
     void displayReservations() const;
+
+
     int getCount() const;
 };
+
 
 #endif
